d79369605d8e3d67600e25d7ebec2074fd064f96
hereiamken <trankien.dang@gmail.com>
2022-01-18 23:17:11 +0700

    no message

---
+++ part3/backend/index.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+const currentTime = () => {
+  var d1 = new Date();
+  var d2 = new Date(
+    d1.getUTCFullYear(),
+    d1.getUTCMonth(),
+    d1.getUTCDate(),
+    d1.getUTCHours().toLocaleString(),
+    d1.getUTCMinutes(),
+    d1.getUTCSeconds(),
+    d1.getTimezoneOffset()
+  );
+
+  return d2.toUTCString();
+};
+
+app.get("/info", (request, response) => {
+  response.send(
+    `<p>Phonebook has info ${persons.length} people</p>
+    <p>${new Date()}</p>`
+  );
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+
+  response.json(person);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
